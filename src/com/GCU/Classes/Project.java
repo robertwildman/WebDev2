d75a7e2b9924a2d06b7d81099f0ad1132f099aa2
Robert Wildman <robertwildman4@gmail.com>
2019-03-28 13:14:43 +0000

    Adding new Project Class
    Signed-off-by: Robert Wildman <robertwildman4@gmail.com>

---
+++ src/com/GCU/Classes/Project.java
@@ -0,0 +1,56 @@
+package com.GCU.Classes;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Project {
+    private int id;
+    private String name,description;
+    private Date ExpectedDone,actualDone;
+
+    protected Project()
+    {
+        Random rand = new Random();
+        this.id =  rand.nextInt(100000);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getExpectedDone() {
+        return ExpectedDone;
+    }
+
+    public void setExpectedDone(Date expectedDone) {
+        ExpectedDone = expectedDone;
+    }
+
+    public Date getActualDone() {
+        return actualDone;
+    }
+
+    public void setActualDone(Date actualDone) {
+        this.actualDone = actualDone;
+    }
+}
