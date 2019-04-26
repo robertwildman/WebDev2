9024f63c0b69530ed016c0a9255a80898bb5a7ad
Robert Wildman <robertwildman4@gmail.com>
2019-04-26 20:09:26 +0100

    Templating Engine Added
    Adding EJS to the system to allow for templating.
    Add new folders which is where the server web pages will be hosted.
    Site/Pages will have each page inside and Site/Section will have reuseable code sections inside them.

--- Routes/accounts.js
+++ Routes/accounts.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // Add a binding to handle '/api/account
 router.get('/', function(req, res){
     // render the '/api/account' view
-    res.send('Accounts');
+    res.render('Pages/index', {message: 'Accounts'});
   })
 
 module.exports = router;
