b5043933b894a312fd301b4776d4b55658b6b2e8
calumMcD <47326204+calumMcD@users.noreply.github.com>
2019-04-27 15:55:57 +0100

    re added server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+var milestoneRoute = require('./Routes/milestone');
+var accountRoute = require('./Routes/accounts');
+var projectRoute = require('./Routes/project');
+
+// Import my test routes into the path '/test'
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/Site'));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use('/api/milestone', milestoneRoute);
+app.use('/api/account', accountRoute);
+app.use('/api/project', projectRoute);
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!');
+});
