6cd494d00ca677b57cbe28778c270b8e3a2f36be
Robert Wildman <robertwildman4@gmail.com>
2019-04-28 22:42:26 +0100

    Adding Login to an account

--- Routes/accounts.js
+++ Routes/accounts.js
@@ -20,25 +20,46 @@ var config =
         encrypt: true
     }
 }
-
-    
+var connection = new Connection(config); 
+connection.on('connect', function(err) {  
+  if (err) {  
+    console.log(err); 
+  }
+});   
     
 // Add a binding to handle '/api/account
 router.get('/', function(req, res){
     // render the '/api/account' view
     res.render('Pages/index', {message: 'Accounts'});
 })
+//Get account with a ID
+router.get('/:id', function(req, res){
+  // render the '/api/account' view
+  var id = req.params.id;
+  var request = new Request(
+    "SELECT * FROM Accounts  WHERE AccountID="+id+";",
+    function(err, rowCount, rows)
+    {
+        console.log(rowCount + ' row(s) returned');
+    }
+  );
+  request.on('row', function(columns) {
+          var account = {
+              AccountID: columns[0].value,
+              Username: columns[1].value,
+              Password: columns[2].value
+          };
+          console.log(account);
+  });
+  connection.execSql(request);
+  res.send(id);
+});
+//Create an account
 router.post('/create', function(req, res) {
   console.log("Incomming Request");
   var bb = req.query.Username,
       cc = req.query.Password;
       console.log("Username: " + bb);
-      var connection = new Connection(config); 
-      
-      connection.on('connect', function(err) {  
-        if (err) {  
-          console.log(err); 
-        }
         // If no error, then good to proceed.  
         request = new Request("INSERT Accounts (Username, Password) VALUES (@Username, @Password);", function(err) {  
           if (err) {  
@@ -49,15 +70,38 @@ router.post('/create', function(req, res) {
          request.on('row', function(columns) {  
              columns.forEach(function(column) {  
                if (column.value === null) {  
-                 console.log('NULL');  
+                 console.log('NULL'); 
                } else {  
                  console.log("Product id of inserted item is " + column.value);  
                }  
              });  
          });       
          connection.execSql(request);  
-       res.send("Done");
-        }); 
-          
+         res.send("Done");
 });
+//Login to an account
+router.post('/login', function(req, res){
+  // render the '/api/account' view
+  var username = req.query.username;
+  var password = req.query.password;
+  console.log(username);
+  var request = new Request(
+    "SELECT * FROM Accounts  WHERE Username='"+username+"';",
+    function(err, rowCount, rows)
+    {
+        console.log(rowCount + ' row(s) returned');
+    }
+  );
+  request.on('row', function(columns) {
+           if(password == columns[2].value)
+           {
+              return res.render('Pages/index', {message: 'Login Successfull'});
+           }else
+           {
+             return res.render('Pages/index', {message: 'Login Failed'});
+           }
+  });
+  connection.execSql(request);
+});
+
 module.exports = router;
