47ac246231a014fdb2af75946c8b6d0dce4a9981
calumMcD <47326204+calumMcD@users.noreply.github.com>
2019-04-29 14:05:13 +0100

    adding delete function for projects

--- Routes/project.js
+++ Routes/project.js
@@ -1,15 +1,15 @@
 var express = require('express')
 var router = express.Router();
-var Request = require('tedious').Request;  
-var TYPES = require('tedious').TYPES;  
-var Connection = require('tedious').Connection;  
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+var Connection = require('tedious').Connection;
 // Create connection to database
 var config =
 {
     authentication: {
         options: {
-            userName: 'wpd2@wpd2', 
-            password: 'YJO4t3eaTwpC' 
+            userName: 'wpd2@wpd2',
+            password: 'YJO4t3eaTwpC'
         },
         type: 'default'
     },
@@ -25,8 +25,8 @@ router.get('/', function(req, res){
     // render the '/api/milestone' view
     res.render('Pages/index', {message: 'Project'});
   })
-  
-  
+
+
   router.get('/PCreate', function(req, res) {
 		console.log("Incomming Request");
 		var PName = req.query.ProjectName,
@@ -36,34 +36,60 @@ router.get('/', function(req, res){
 			PUserID = req.query.ProjectUserID;
 			var connection = new Connection(config);
 			connection.on('connect', function(err) {
-				if (err) {  
-          console.log(err); 
+				if (err) {
+          console.log(err);
 			}
 			// If no error, then good to proceed.
-			request = new Request("INSERT Project (ProjectName, ProjectDesc, ProjectDue, ProjectComp, ProjectUserID) VALUES (@ProjectDesc, @ProjectDue, @ProjectComp, @ProjectUserID);", function(err) { 
-			  if (err) {  
+			request = new Request("INSERT Project (ProjectName, ProjectDesc, ProjectDue, ProjectComp, ProjectUserID) VALUES (@ProjectDesc, @ProjectDue, @ProjectComp, @ProjectUserID);", function(err) {
+			  if (err) {
              console.log(err);}
-			});  
-			
-			request.addParameter('ProjectName', TYPES.NVarChar,PName); 
+			});
+
+			request.addParameter('ProjectName', TYPES.NVarChar,PName);
 			request.addParameter('ProjectDesc', TYPES.NVarChar,PDesc);
 			request.addParameter('ProjectDue', TYPES.NVarChar,PDateDue);
 			request.addParameter('ProjectComp', TYPES.NVarChar,PDateComp);
 			request.addParameter('ProjectUserID', TYPES.NVarChar,PUserID);
-			request.on('row', function(columns) {  
-             columns.forEach(function(column) {  
-               if (column.value === null) {  
-                 console.log('NULL');  
-               } else {  
-                 console.log("Product id of inserted item is " + column.value);  
-               }  
-             });  
-         });       
+			request.on('row', function(columns) {
+             columns.forEach(function(column) {
+               if (column.value === null) {
+                 console.log('NULL');
+               } else {
+                 console.log("Product id of inserted item is " + column.value);
+               }
+             });
+         });
          connection.execSql(request);
        res.send("Done");
-        }); 
-			
+        });
+
 });
 
-module.exports = router;
+  router.get('/PDelete', function(req, res){
+    console.log("Incomming Request");
+    var ProjectID = req.query.ProjectID;
+    var connection = new Connection(config);
+    connection.on('connect', function(err){
+      if (err) {
+        console.log(err);}
+    }
+    // if there is no conenction error, then proceed
+    request = new Request("DELETE FROM Projects , Milestones WHERE Project.PROJECTID = Milestone.ProjectID AND ProjectID ='@ProjectID'", function(err){
+      if (err) {
+        console.log(err);}
+    });
+    request.deleteParameter('ProjectID', TYPES.NVarChar,ProjectName);
+    request.on('row', function(columns){
+      if ()column.value==null){
+        console.log('NULL');
+      } else {
+        console.log("The project has now been deleted");
+      }
+    });
+    connection.execSql(request);
+    res.send("Done");
+   });
 
+  });
+
+module.exports = router;
